4e823fc1e7b4b08331bcfb529a2ec80bd196c27f
liza_kryachko <krckoliza095@gmail.com>
2026-02-25 19:42:25 +0200

    1

--- main.py
+++ main.py
@@ -1,52 +1,56 @@
-import requests
 from config import Config, load_config
-from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, F
 import asyncio
-from aiogram.filters import Command, CommandObject
+from pathlib import Path
+from aiogram.types import Message,FSInputFile
 config: Config = load_config()
 bot_token = config.bot.token
 print(bot_token)
 bot = Bot(token=bot_token)
-
-
 dp = Dispatcher()
+'''
+#домашняя папка пользоваеля
+HOME_DIR = Path.home()
+#раюочий стол
+DESKTOP_DIR = HOME_DIR / "Desktop"
+#папка
+DOWNLOAD_DIR = DESKTOP_DIR / "Download"
 
-@dp.message(Command(commands=['start']))
-async def process_start(message: Message):
-    await message.answer('Привет я твой первый бот')
+@dp.message(F.photo)
+    #bot -
+async def photo_handler(message: Message, bot: Bot):
+    photo = message.photo[-1]
+    file = await bot.get_file(photo.file_id)
+    await bot.download_file(file.file_path, DOWNLOAD_DIR)
+'''
+'''
+@dp.message(F.text.lower()=='гони фото')
+async def song_photo_url(message: Message):
+    #нельзя отправить текст> 4096
+    #нельзя отправить caption > 1024
+    photo = 'https://yandex.ru/images/search?img_url=https%3A%2F%2Fupload-os-bbs.hoyolab.com%2Fupload%2F2025%2F01%2F22%2F134246182%2Fe701c77c5c1cc2866877cb95ee845f6c_6446799511393735519.webp&lr=22&pos=0&rpt=simage&source=serp&text=phainon'
+    await message.answer_photo(
+        photo,
+        caption = 'во псина'
+    )
+    #отправка локального файла
+    photo = FSInputFile('files/test_1.jpg')
+    await message.answer_photo(
+        photo = photo,
+        caption = 'во фото'
+    )
+'''
 
-@dp.message(Command(commands=['help']))
-async def process_help(message: Message):
-    await message.answer("чем могу помочь?")
-
-@dp.message(Command(commands=['breeds']))
-async def show_breeds(message: Message):
-    result = requests.get('https://dog.ceo/api/breeds/list/all')
-    result_json = result.json()
-    dogs_types = result_json['message'].keys()
-    s = '\n'.join(list(dogs_types)[:30])
-    await message.answer(s)
-
-@dp.message(Command(commands=['dog']))
-async def process_dog(message: Message, command: CommandObject):
-    if command.args:
-        s = requests.get(f'https://dog.ceo/api/breed/{command.args}/images/random')
-    else:
-      s = requests.get('https://dog.ceo/api/breeds/image/random')
-    json_s = s.json()
-#requests + bs4
-    if json_s.get('status') == 'success':
-        #print(json_s.get('message'))
-        await message.answer_photo(photo = json_s.get('message'))
-    else:
-        await message.answer('неправильно :( я не могу так ')
+@dp.message(F.photo)
+async def photo_reply(message: Message):
+    photo = message.photo[-1]
+    await message.reply_photo(
+        photo = photo.file_id,
+        caption = 'мда не кидай мне больше эту фигню'
 
 
-
-    print(s.content)
-
+    )
+    # пользователь отправляет фото, вернуть это же фото
 
 async def main():
 
