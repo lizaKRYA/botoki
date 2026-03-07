c26e9af54ae8a4205199bbd9551c77ef85a2e41a
liza_kryachko <krckoliza095@gmail.com>
2026-03-07 19:48:58 +0200

    1

--- main.py
+++ main.py
@@ -10,48 +10,29 @@ from requests import get
 async def main():
  bot = Bot(token=BOT_TOKEN)
  dp = Dispatcher()
-#
- @dp.message (Command(commands=['catfact']))
- async def cat_fact(message: Message):
-     print(f'[LOG] user {message.from_user.id} used command /catfact')
-     print(f'[LOG] запрашиваю факт о котах')
-     response = get('https://catfact.ninja/fact')
-     print(f'[LOG] получен результат со статусом {response.status_code}')
-     response_json = response.json()
-     print(response_json['fact'])
 
-     await message.answer(response_json['fact'])
-
-
-
-
- @dp.message(Command(commands = ['breed']))
- async def breeding(message: Message):
-     print(f'[LOG] пользователь {message.from_user.id} нажал команду /breed')
-     print(f'[LOG] запрашиваю породу кота')
-     response = get('https://catfact.ninja/breeds')
-     print(f'[LOG] получен результат со статусом {response.status_code}')
-     response_json = response.json()
-
-     result_item = choice(response_json['data'])
-     country = choice(response_json['data'])
-     result = (f'случайная порода: {result_item['breed']} \n'
-               f'родина: {result_item['country']}')
-     await message.answer(result)
-     print(f'[LOG] запрос успешно отправлен пользователю {message.from_user.id}')
-
-
+ @dp.message(F.text.contains('start'))
+ async def start_handler(message: Message):
+   await message.reply(f'ahoj {message.from_user.full_name}')
  @dp.message(Command(commands=['start']))
  async def start(message: Message):
      print(f'[LOG] пользователь {message.from_user.id} нажал кнопку /start')
      await message.answer(f'привет {message.from_user.full_name}')
 
- @dp.message(F.text)
- async def cat_text(message: Message):
-     print(f'[LOG] пользователь {message.from_user.id} написал текст')
-     print(f'[LOG] фильрую данный текст')
-     await message.answer('оч интересно')
-     print(f'[LOG] соо успешно обработалось со статусом {F.status_code}')
+
+ s ={5513935927, 6574836746, 1234567890}
+ @dp.message( Command(commands= ['secret']), F.from_user.id.in_(s))
+ async def secret(message: Message):
+     await message.answer(' ahoj ! ja som oki bot')
+ @dp.message(not(F.user_name.id == 5513935927 | 6574836746 | 1234567890))
+ async def message(message: Message):
+     await message.answer(' no')
+
+
+
+
+
+
 
  await dp.start_polling(bot)
 
