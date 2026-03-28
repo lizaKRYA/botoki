0efc118b1748e168ee92b2e6c7f4bc012ec10d69
lizaKRYA <elizavetakryachko@icloud.com>
2026-03-28 20:46:14 +0300

    1

--- main.py
+++ main.py
@@ -1,58 +1,82 @@
-import requests
-from config import Config, load_config
-from aiogram import Bot, Dispatcher
+import types
+from random import choice
+from config import BOT_TOKEN
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message , KeyboardButton, ReplyKeyboardMarkup, BotCommand, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from aiogram import Bot, Dispatcher,F
 import asyncio
-from aiogram.filters import Command, CommandObject
-config: Config = load_config()
-bot_token = config.bot.token
-print(bot_token)
-bot = Bot(token=bot_token)
-
-
-dp = Dispatcher()
-
-@dp.message(Command(commands=['start']))
-async def process_start(message: Message):
-    await message.answer('Привет я твой первый бот')
-
-@dp.message(Command(commands=['help']))
-async def process_help(message: Message):
-    await message.answer("чем могу помочь?")
-
-@dp.message(Command(commands=['breeds']))
-async def show_breeds(message: Message):
-    result = requests.get('https://dog.ceo/api/breeds/list/all')
-    result_json = result.json()
-    dogs_types = result_json['message'].keys()
-    s = '\n'.join(list(dogs_types)[:30])
-    await message.answer(s)
-
-@dp.message(Command(commands=['dog']))
-async def process_dog(message: Message, command: CommandObject):
-    if command.args:
-        s = requests.get(f'https://dog.ceo/api/breed/{command.args}/images/random')
-    else:
-      s = requests.get('https://dog.ceo/api/breeds/image/random')
-    json_s = s.json()
-#requests + bs4
-    if json_s.get('status') == 'success':
-        #print(json_s.get('message'))
-        await message.answer_photo(photo = json_s.get('message'))
-    else:
-        await message.answer('неправильно :( я не могу так ')
-
-
-
-    print(s.content)
-
+from time import sleep
 
 async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+
+    btn_1 = InlineKeyboardButton(
+        text = 'pizza',
+        callback_data = 'food_1'
+    )
+
+    btn_2 = InlineKeyboardButton(
+        text = 'sushi',
+        url  = 'food_2'
+    )
+
+    s = []
+    for i in range(1):
+        s.append(btn_1)
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard= s
+    )
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[[btn_1], [btn_2]]
+    )
+
+    @dp.message(F.text)
+    async def text(message: Message):
+        await message.answer(
+            f'вот клава',
+            reply_markup=keyboard
+        )
+
+    @dp.callback_query(F.data.startwith('food'))
+    async def callback_handler(callback: CallbackQuery):
+        await callback.edit_text('ваш заказ оформлен')
+#callback.answer
+#callback.message.answer()
+#awat callback.message.edit_text('ваш заказ оформлен")
+    s = [
+        'вам отправиди пиццу',
+        'вам отправили суши'
+    ]
+
+    data = callback.data
+    slited_data = data.split('-')
+    index = int(slited_data[1]) - 1
+    await callback.message.answer(s[data])
+
+
+
+    s = {
+        'food_1': 'pizza',
+        'food_2': 'sushi'
+    }
+
+    data = callback.data
+    await callback.message.answer(f'вы выбрали{s[data]}')
+    await asyncio.sleep(1)
+    await callback.answer('оформлен заказ')
+    await asyncio.sleep(1)
+    await callback.message.edit_text('ожидание курьера')
+    await asyncio.sleep(1)
+    await callback.message.edit_text('заказ готов')
+
+
 
     await dp.start_polling(bot)
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
 
+print(f'[LOG] Бот запущен')
+asyncio.run(main()) # запускает цикл событий (dp)
